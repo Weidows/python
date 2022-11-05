88372098388469dc8e55f058d5ec4c6db2f8a9fd
Weidows <utsuko27@qq.com>
2022-11-05 15:30:56 +0800

    init.

---
+++ utils/files.py
@@ -0,0 +1,16 @@
+import csv
+
+
+def read_csv(filename: str):
+  l=[]
+  with open(filename, encoding='utf-8') as fp:
+      reader = csv.reader(fp)
+      # 获取标题
+      header = next(reader)
+      # 遍历数据
+      for i in reader:
+          m={}
+          for j in range(len(header)) :
+            m[header[j]]=i[j]
+          l.append(m)
+  return l
